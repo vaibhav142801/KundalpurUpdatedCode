a5ef639f5247660c8a64f135acb8438c4ce011bb
ravikant dewangan <ravidewangan13@gmail.com>
2022-12-12 21:37:49 +0530

    auth add

--- Efilling_backend/src/routes/v1/user.routes.js
+++ Efilling_backend/src/routes/v1/user.routes.js
@@ -4,13 +4,14 @@ const validate = require('../../middlewares/validate');
 const { userValidation } = require('../../validations');
 const router = express.Router();
 const passport = require('passport');
+const auth = require('../../middlewares/auth');
 
 router.route('/login-with-mobile').post(validate(userValidation.loginMobile),userController.loginWithMobile);
 router.route('/login-with-email').post(validate(userValidation.loginEmail),userController.loginWithEmail);
 router.route('/verify-opt').post(userController.verifyOTP);
 router.route('/forgot-password').post(validate(userValidation.forgotPass),userController.forgotPassword);
-// router.route('/add-cash-donation').post(passport.authenticate('jwt', { session: false }),donationController.addCashDonation);
-router.route('/add-cash-donation').post(donationController.addCashDonation);
+router.route('/add-cash-donation').post(auth(),donationController.addCashDonation);
+// router.route('/add-cash-donation').post(donationController.addCashDonation);
 router.route('/donation-list').get(donationController.donationList);
 
 module.exports = router;
